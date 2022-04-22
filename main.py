97061a8621c03acdcf9f4037a65f7e73d1c91809
vasily.dergilev <vasily.dergilev@gmail.com>
2022-04-23 00:10:12 +0300

    minor fix

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import pygame
 import sys
+
 pygame.init()
 
 size = width, height = 320, 240
 speed = [2, 2]
-black = 0, 0,
+black = 0, 0, 0
 
 
 screen = pygame.display.set_mode(size)
@@ -13,7 +14,8 @@ ballrect = ball.get_rect()
 
 while 1:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT: sys.exit()
+        if event.type == pygame.QUIT:
+            sys.exit()
 
     ballrect = ballrect.move(speed)
     if ballrect.left < 0 or ballrect.right > width:
