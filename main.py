62fbf78a0f4c1ef68eec471be14e3544fc65ef49
vasily.dergilev <vasily.dergilev@gmail.com>
2022-04-23 00:51:44 +0300

    minor fix

--- main.py
+++ main.py
@@ -17,10 +17,10 @@ while 1:
 
     ballrect = ballrect.move(speed)
     if ballrect.left < 0 or ballrect.right > width:
-        speed[0] = min(-speed[0] * 3, size[0] * -1 if speed[0] > 0 else 1)
+        speed[0] = min(-speed[0] * 3, size[0] * (-1 if speed[0] > 0 else 1))
 
     if ballrect.top < 0 or ballrect.bottom > height:
-        speed[1] = min(-speed[1] * 3, size[0] * -1 if speed[1] > 0 else 1)
+        speed[1] = min(-speed[1] * 3, size[0] * (-1 if speed[1] > 0 else 1))
 
     screen.fill(black)
     screen.blit(cock, ballrect)
